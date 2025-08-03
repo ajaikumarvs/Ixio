e234952e8a0a94e8153ae4b07fc3a6cc251cd804
ajaikumarvs <ajaikumarvs@gmail.com>
2025-08-03 19:42:17 +0530

    cleanup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,5 +10,5 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error while running the application");
 }
